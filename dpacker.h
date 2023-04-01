27b6fb47c0130acd209d5614380b3e86b45bd877
Dhruv Sharma <dhruv4jaipur@gmail.com>
2023-04-01 14:37:51 +0530

    Update dpacker.h

--- dpacker.h
+++ dpacker.h
@@ -78,7 +78,7 @@ namespace dpacker {
     };
     
 
-    struct MeshDec1
+    struct MeshDecl
     {
         const void *vertexPositionData = nullptr;
         const void *vertexNormalData = nullptr;
@@ -106,8 +106,91 @@ namespace dpacker {
             Error,
             IndexOutOfRange,
             InvalidIndexCount,
-        }
+        };
     };
     
+
+	AddMeshError::Enum AddMesh(Packer *packer, const MeshDecl &meshDecl, uint32_t meshCountHint = 0);
+
+    void AddMeshJoin(Packer *packer);
+
+    struct UvMeshDcal
+    {
+        const void *vertexPositionData = nullptr; 
+        const void *vertexUvData = nullptr;
+        const void *indexData = nullptr;
+        const uint32_t *faceMaterialData = nullptr;
+        uint32_t vetexCount = 0;
+        uint32_t vertexPositionStride = 0;
+        uint32_t vertexStride = 0;
+        uint32_t indexCount = 0;
+        int32_t indexOffset = 0;
+        IndexFormat::Enum indexFormat = IndexFormat::UInt16;
+        bool rotateCharts  = true;
+    };
+
+    AddMeshError::Enum AddUvMesh(Packer *packer, const UvMeshDcal &decl);
+
+    typedef void(*ParameterizeFunc)(const float *positions, float *texcoords, uint32_t vertexCount, const uint32_t *indices, uint32_t indexCount);
+
+         struct ChartOptions
+         {
+            float maxChartArea = 0.0f;
+            float maxBoundaryLength = 0.0f;
+            float normalDeviationweight = 2.0f;
+            float roundnessWeight = 0.01f;
+            float straightnessWeight = 6.0f;
+            float normalSeamWeight = 0.5f;
+            float maxCost = 2.0f;
+            uint32_t maxIterations = 1;
+            ParameterizeFunc paramFunc = nullptr;
+            bool closeHoles = false;
+            bool fixTJunctions = true;
+         };
+    
+    void ComputeCharts(Packer *packer, ChartOptions options = ChartOptions());
+
+    struct PackOptions
+    {
+        bool bilinear = true;
+        bool blockAlign = false;
+        bool bruteForce = false;
+        bool createImage = false;
+        
+        uint32_t maxChartSize = 0;
+        uint32_t padding = 0;
+        float texelsPerUnit = 0.0f;
+        uint32_t resolution = 0;
+    };
+
+    void PackCharts(Packer *packer, PackOptions packOptions = PackOptions());
+
+    void Generate(Packer *packer, ChartOptions chartOptions = ChartOptions(), PackOptions packOptions = PackOptions());
+
+    struct ProgressCategory
+    {
+        enum Enum
+        {
+            AddMesh,
+            ComputeCharts,
+            ParameterizeCharts,
+            PackCharts,
+            BuildOutputMeshes,
+        };
+    };
+    
+    typedef bool(*ProgressFunc)(ProgressCategory::Enum category, int progress, void *userData);
+
+    void SetProgressCallBack(Packer *packer, ProgressFunc pregressFunc = nullptr, void *progressUserData = nullptr);
+
+    typedef void *(*ReallocFunc)(void *, size_t);
+    typedef void(*FreeFunc)(void *);
+    void SetAlloc(ReallocFunc reallocFunc, FreeFunc freeFunc = nullptr);
+    typedef int(*PrintFunc)(const char *, ...);
+    void SetPrint(PrintFunc print, bool verbose);
+    const char *StringForEnum(AddMeshError::Enum error);
+    const char *StringForEnum(ProgressCategory::Enum category);
     
 }
+
+#endif
